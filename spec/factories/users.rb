463d08ab2ffc14bf0a1dd22b322215bd94a605dc
prospike <best_diagram22@hotmail.co.jp>
2023-10-29 14:51:59 +0900

    レビュー対応

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'test' }
     email                 { 'test@example' }
-    password              { 'aaa000' }
+    password              { Faker::Internet.free_email }
     password_confirmation { password }
     family_name           { '山田' }
     first_name            { '太郎' }
