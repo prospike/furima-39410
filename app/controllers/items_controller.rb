577b1bd065615f463be711bb29ac82ad1c51bc95
prospike <best_diagram22@hotmail.co.jp>
2023-10-29 13:54:37 +0900

    validatesの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+		params.require(:item).permit(:image_name,:image,:description,:category,:condition,:prefecture,:delivery_fee,:shipping_date,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,28 @@ class Item < ApplicationRecord
   belongs_to :shipping_date
   has_one_attached :image
 
+  validates :item_name, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, presence: true
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, presence: true
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_date_id, presence: true
+  validates :shipping_date_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+  validates :price, format: { with: /\A\d+\z/, message: "は半角数値のみ入力してください。" }
+  validate :price_range
 
+  private
+
+  def price_range
+    if price.present? && (price < 300 || price > 9_999_999)
+      errors.add(:price, 'は¥300から¥9,999,999の範囲内である必要があります。')
+    end
+  end
 end
