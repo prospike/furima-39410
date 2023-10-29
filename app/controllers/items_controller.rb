3369f315a0d5d78ad853d7e29570c440fa7d1a27
prospike <best_diagram22@hotmail.co.jp>
2023-10-29 16:51:42 +0900

    ログインログアウト分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
+	
 	def index
   end
 
@@ -7,13 +9,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(tweet_params)
-  end
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+	end
 
 
   private
 
   def item_params
-		params.require(:item).permit(:image_name,:image,:description,:category_id,:condition_id,:prefecture_id,:delivery_fee_id,:shipping_date_id,:price).merge(user_id: current_user.id)
+		params.require(:item).permit(:item_name,:image,:description,:category_id,:condition_id,:prefecture_id,:delivery_fee_id,:shipping_date_id,:price).merge(user_id: current_user.id)
   end
 end
