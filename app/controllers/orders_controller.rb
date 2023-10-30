c3f103c27b2e2f5d71d30319083cd36a37561ab2
prospike <best_diagram22@hotmail.co.jp>
2023-10-30 15:39:26 +0900

    カード決済add途中

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,8 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    if @order_address.valid? 
+    if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -26,8 +27,16 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:post_num, :prefecture_id, :city, :street_number, :building_name, :phone_num ).merge(user_id: current_user.id, item_id: @item.id )
+    params.require(:order_address).permit(:post_num, :prefecture_id, :city, :street_number, :building_name, :phone_num ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token], price: @item.price )
   end
 
+	def pay_item
+    Payjp.api_key = "sk_test_509a3b3fd18603fe1b0214ea"
+      Payjp::Charge.create(
+        amount: order_params[:price],
+        card: order_params[:token],
+        currency: 'jpy'
+      ) 
+  end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_num, :prefecture_id, :city, :street_number, :building_name, :phone_num, :item_id, :user_id
+  attr_accessor :post_num, :prefecture_id, :city, :street_number, :building_name, :phone_num, :item_id, :user_id, :token, :price
 
 	
 	with_options presence: true do
