4fd43d325c0ef90aa23ab4748825328fee78acb2
prospike <best_diagram22@hotmail.co.jp>
2023-10-30 15:10:07 +0900

    validate

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,9 +2,14 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :post_num, :prefecture_id, :city, :street_number, :building_name, :phone_num, :item_id, :user_id
 
-  with_options presence: true do
+	
+	with_options presence: true do
     validates :user_id
     validates :post_num, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    #validates :prefecture_id, numericality: {other_than: 1 }
+    validates :city
+    validates :street_number
+    validates :phone_num, format: {with: /\A[0-9]{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください"}
   end
   validates :prefecture_id, numericality: {other_than: 1 }
 
