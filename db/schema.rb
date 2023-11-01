c795a39ba5fa30d7b0429a2bafd1af29c865f1e7
prospike <best_diagram22@hotmail.co.jp>
2023-11-01 17:23:44 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_30_031800) do
+ActiveRecord::Schema.define(version: 2023_10_30_045704) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -74,6 +74,11 @@ ActiveRecord::Schema.define(version: 2023_10_30_031800) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "order_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
